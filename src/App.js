bae7a0b96217ae42430922bff93c89bfe375826d
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-04-25 16:39:09 -0400

    added background image, button, and label

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
 import GlobalStyle from './components/styles/GlobalStyle';
+import Header from './components/Header';
+import Hero from './components/Hero';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <GlobalStyle />
       <Header />
+      <Hero />
     </>
   );
 }
