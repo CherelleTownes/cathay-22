61165fa08ef4436713c145e9497887246aa04944
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-04-25 12:52:22 -0400

    clean file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Route, Link } from "react-router-dom";
 import './App.css';
 
-// https://agitated-tereshkova-ead074.netlify.app/
+
 
 function App() {
   return (
