81166fb7be0624b011289da1c23f54819edf5f81
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-04-25 15:03:41 -0400

    uploaded images and menus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import './App.css';
-
-
+import Header from './components/Header';
+import GlobalStyle from './components/styles/GlobalStyle';
+import PDF from './components/PDF';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Header />
       <h1>hello</h1>
       <a href="tel:+1-347-276-0169">Test</a>
+      <PDF />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header` 
+  display: flex;
+  background-color: #a32e02;
+  height: 169px;
+`;
+
+export default function Header() {
+  return <StyledHeader />
+}
