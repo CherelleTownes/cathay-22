aafdcf5cc31fc77f71f4266855f2e236b451771f
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-04-25 15:39:50 -0400

    added nav and a tags to header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import GlobalStyle from './components/styles/GlobalStyle';
-import PDF from './components/PDF';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
       <Header />
-      <h1>hello</h1>
-      <a href="tel:+1-347-276-0169">Test</a>
-      <PDF />
-    </div>
+    </>
   );
 }
 
