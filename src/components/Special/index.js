0bc634e3e62c981f00cbb5a8a7753430d343912d
kay <kaych26@gmail.com>
2020-04-28 15:30:15 -0400

    response query for special events

--- src/components/Special/index.js
+++ src/components/Special/index.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Modal from '../Modal';
+import { useMediaQuery } from 'react-responsive';
 import special from '../../assets/images/special-events.png';
 import specialMenu from '../../assets/menus/new-year-menu.png';
 
 const StyledSpecial = styled.section`
   height: 627px;
   background-color: #a32e02;
-  /* display: grid;
-  grid-template-columns: 156px 606px 83px 377px 217px;
-  grid-template-rows: 136px 25px 230px 152px 83px; */
+  @media (max-width: 375px) {
+    height: 207px;
+  }
 `;
 
 const Layout = styled.div`
   display: grid;
-  grid-template-columns: 156px 606px 84px 377px 217px;
-  grid-template-rows: 136px 25px 230px 152px 83px;
+  /* grid-template-columns: 156px 606px 84px 377px 217px;
+  grid-template-rows: 136px 25px 230px 152px 83px; */
+  grid-template-columns: 11% 42% 6% 26% 15%;
+  grid-template-rows: 21% 5% 37% 24% 13%;
   height: 100%;
   max-width: 1440px;
   margin: 0 auto;
+  @media (max-width: 375px) {
+    max-width: 375px;
+    grid-template-columns: 8% 54% 4% 28% 6%;
+    grid-template-rows: 21% 6% 26% 20% 27%;
+  }
 `;
 
 const SpecialImg = styled.img`
@@ -27,6 +35,11 @@ const SpecialImg = styled.img`
   border-radius: 15px;
   grid-area: 1 / col4-start / last-line / 4;
   align-self: center;
+  @media (max-width: 375px) {
+    width: 107px;
+    height: 156px;
+    border-radius: 15px;
+  }
 `;
 
 const Label = styled.h1`
@@ -43,10 +56,13 @@ const Label = styled.h1`
   grid-row: 1;
   justify-self: end;
   align-self: end;
+  @media (max-width: 375px) {
+    font-size: 13px;
+    letter-spacing: 0.65px;
+  }
 `;
 
 const ContainerText = styled.div`
-  width: 650px;
   height: 156px;
   grid-column: 2;
   grid-row: 3;
@@ -63,7 +79,14 @@ const Text = styled.p`
   color: #ffffff;
   padding-bottom: 20px;
   text-align: right;
+  @media (max-width: 375px) {
+    font-size: 8px;
+    line-height: 1;
+    letter-spacing: 0.24px;
+    padding-bottom: 8px;
+  }
 `;
+
 const ContainerButton = styled.div`
   width: 580px;
   height: 152px;
@@ -75,10 +98,17 @@ const ContainerButton = styled.div`
   flex-direction: row;
   justify-content: space-around;
   align-items: flex-end;
+  @media (max-width: 375px) {
+    width: 195px;
+    height: 30px;
+    grid-row: 5;
+    justify-content: space-between;
+  
+  }
 `;
 
 const Button = styled.button`
-  width: ${(props) => props.width};
+  width: ${(props) => props.width}px;
   height: 60px;
   border-radius: 44px;
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.4);
@@ -90,6 +120,13 @@ const Button = styled.button`
   line-height: normal;
   letter-spacing: 0.9px;
   color: #000000;
+  @media (max-width: 375px) {
+    /* width: ${(props) => props.width - 93}px; */
+    height: 21px;
+    font-size: 8px;
+    line-height: normal;
+    letter-spacing: 0.4px;
+  }
 `;
 
 export default function Special() {
@@ -109,6 +146,9 @@ export default function Special() {
     height: '95%',
     width: '60%',
   };
+  const isMobile = useMediaQuery({ query: '(max-width: 375px)' });
+  const buttonWidthCall = isMobile ? 64 : 175;
+  const buttonWidthMenu = isMobile ? 111 : 204;
 
   return (
     <StyledSpecial id="special-events">
@@ -130,15 +170,18 @@ export default function Special() {
         </ContainerText>
         <ContainerButton>
           <a href="tel:+1-973-467-8688">
-            <Button width="175px">CALL US</Button>
+            <Button width={buttonWidthCall}>CALL US</Button>
           </a>
+          {/* {!isMobile && ( */}
           <Button
-            width="204px"
+            // width="204"
+            width={buttonWidthMenu}
             onClick={(e) => handleOpen(e)}
             name={specialMenu}
           >
             CHINESE NEW YEAR MENU '20
           </Button>
+          {/* )} */}
           {isOpen && <Modal image={menu} close={handleClose} size={menuSize} />}
         </ContainerButton>
         <SpecialImg src={special} />
