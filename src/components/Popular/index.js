65450f6d6e078223fad8d137ba064a1638929926
Jose Ferreira <jferreira590@gmail.com>
2020-04-29 23:04:34 -0400

    Adding custom arrow buttons and responsiveness to the carousel.

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -5,13 +5,16 @@ import 'react-multi-carousel/lib/styles.css';
 
 import Modal from '../Modal';
 import DishImage from './DishImage';
+import { CustomLeftArrow, CustomRightArrow } from './customArrows';
 import dishes from './dishes';
-import leftArrow from '../../assets/images/arrows/carousel-arrow-left.png';
-import rightArrow from '../../assets/images/arrows/carousel-arrow-right.png';
 
-const StyledPopular = styled.section`
+const StyledSection = styled.section`
   height: 684px;
   background-color: #f4eaea;
+
+  @media (max-width: 414px) {
+    height: 176px;
+  }
 `;
 
 const Grid = styled.div`
@@ -19,6 +22,12 @@ const Grid = styled.div`
   grid-template-rows: 41px 352px;
   grid-row-gap: 65px;
   padding: 78px 0 148px 0;
+
+  @media (max-width: 414px) {
+    grid-template-rows: 18px 91px;
+    grid-row-gap: 18px;
+    padding: 16px 0 33px 0;
+  }
 `;
 
 const Heading = styled.h2`
@@ -27,6 +36,12 @@ const Heading = styled.h2`
   font-weight: bold;
   letter-spacing: 1.75px;
   color: #000000;
+
+  @media (max-width: 414px) {
+    font-size: 15px;
+    font-weight: bold;
+    letter-spacing: 0.75px;
+  }
 `;
 
 const responsive = {
@@ -64,10 +79,16 @@ export default function Popular() {
   };
 
   return (
-    <StyledPopular>
+    <StyledSection>
       <Grid>
         <Heading>SPECIALS AND POPULAR ITEMS</Heading>
-        <Carousel responsive={responsive} centerMode={true} infinite={true}>
+        <Carousel
+          responsive={responsive}
+          centerMode={true}
+          infinite={true}
+          customRightArrow={<CustomRightArrow />}
+          customLeftArrow={<CustomLeftArrow />}
+        >
           {dishes.map((dish, i) => (
             <DishImage key={i} dish={dish} handleOpen={handleOpen} />
           ))}
@@ -81,6 +102,6 @@ export default function Popular() {
           dish={dish}
         />
       )}
-    </StyledPopular>
+    </StyledSection>
   );
 }
