6cc40284058d54b8157b53643a5dcae2a88d1709
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-05-03 14:11:59 -0400

    test merge to master

--- src/components/shared/Heading.js
+++ src/components/shared/Heading.js
@@ -16,3 +16,23 @@ const Heading = styled.h2`
 `;
 
 export default Heading;
+
+import styled from 'styled-components';
+
+const Heading = styled.h2`
+  justify-self: center;
+  align-self: center;
+  font-size: 34px;
+  font-weight: bold;
+  letter-spacing: 1.75px;
+  color: #000000;
+
+  @media (max-width: 414px) {
+    font-size: 15px;
+    font-weight: bold;
+    letter-spacing: 0.75px;
+  }
+`;
+
+export default Heading;
+
